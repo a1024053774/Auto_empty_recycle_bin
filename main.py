452edb1c3366e55b4a6a88cba1d9163002259f61
LuckyE <1024053774@qq.com>
2024-02-17 15:57:49 +0800

    Auto_empty_recycle_bin
    自动清空回收站python

---
+++ main.py
@@ -0,0 +1,17 @@
+import winshell
+import time
+
+def empty_recycle_bin():
+    try:
+        winshell.recycle_bin().empty(confirm=False, show_progress=False, sound=False)
+        print("Recycle Bin emptied successfully.")
+    except Exception as e:
+        print(f"Error emptying Recycle Bin: {e}")
+
+# 设置清空回收站的间隔时间（秒）
+interval_seconds = 60
+
+while True:
+    empty_recycle_bin()
+    # 等待指定的间隔时间
+    time.sleep(interval_seconds)
